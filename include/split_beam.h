9c1c5daf2376253c585b1fad1acf4c4669f0bc2a
Marian Kassel <mkassel@Marians-MacBook-Pro.local>
2026-09-17 17:49:53 +0200

    Introduce split_beam files

---
+++ include/split_beam.h
@@ -0,0 +1,7 @@
+#ifndef SPLIT_BEAM_H
+#define SPLIT_BEAM_H
+
+#include <stdio.h>
+size_t count_split_beam(const FILE *file);
+
+#endif // !SPLIT_BEAM_H
---
+++ src/split_beam.c
@@ -0,0 +1,8 @@
+#include "split_beam.h"
+#include <stdio.h>
+
+size_t count_split_beam(const FILE *file) {
+  size_t splits = 0;
+
+  return splits;
+}
